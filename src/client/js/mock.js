d87010d76fab1a1b4b6cd352b37152988946309f
Dilip Agheda <dilip_agheda@yahoo.com>
2020-08-21 22:43:43 +1000

    Added mock data for testing

---
+++ src/client/js/mock.js
@@ -0,0 +1,175 @@
+export const apodResponseVideo = {
+    date: '2020-08-19',
+    explanation: 'Does the Sun change as it rotates? Yes, and the changes can vary from subtle to dramatic. In the featured time-lapse sequences, our Sun -- as imaged by NASA\'s Solar Dynamics Observatory -- is shown rotating though an entire month in 2014.  In the large image on the left, the solar chromosphere is depicted in ultraviolet light, while the smaller and lighter image to its upper right simultaneously shows the more familiar solar photosphere in visible light. The rest of the inset six Sun images highlight X-ray emission by relatively rare iron atoms located at different heights of the corona, all false-colored to accentuate differences. The Sun takes just under a month to rotate completely -- rotating fastest at the equator. A large and active sunspot region rotates into view soon after the video starts.  Subtle effects include changes in surface texture and the shapes of active regions. Dramatic effects include numerous flashes in active regions, and fluttering and erupting prominences visible all around the Sun\'s edge.  Presently, our Sun is passing an unusually low  Solar minimum in activity of its 11-year magnetic cycle. As the video ends, the same large and active sunspot region previously mentioned rotates back into view, this time looking different.',
+    media_type: 'video',
+    service_version: 'v1',
+    title: 'The Sun Rotating',
+    url: 'https://www.youtube.com/embed/2WRgXvdasm0'
+  }
+
+  export const apodResponseImage = {
+    "date": "2020-08-18",
+    "explanation": "Do other stars have planets like our Sun? Previous evidence shows that they do, coming mostly from slight shifts in the star's light created by the orbiting planets.  Recently, however, and for the first time, a pair of planets has been directly imaged around a Sun-like star.  These exoplanets orbit the star designated TYC 8998-760-1 and are identified by arrows in the featured infrared image.  At 17 million years old, the parent star is much younger than the 5-billion-year age of our Sun.  Also, the exoplanets are both more massive and orbit further out than their Solar System analogues: Jupiter and Saturn. The exoplanets were found by the ESO's Very Large Telescope in Chile by their infrared glow – after the light from their parent star was artificially blocked.  As telescope and technology improve over the next decade, it is hoped that planets more closely resembling our Earth will be directly imaged.    Experts Debate: How will humanity first discover extraterrestrial life?",
+    "hdurl": "https://apod.nasa.gov/apod/image/2008/TYC8998_ESO_960.jpg",
+    "media_type": "image",
+    "service_version": "v1",
+    "title": "TYC 8998-760-1: Multiple Planets around a Sun Like Star",
+    "url": "https://apod.nasa.gov/apod/image/2008/TYC8998_ESO_960.jpg"
+  }
+
+  export const marsRoverImages = {
+    photos: [
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FRB_651133500EDR_F0822176FHAZ00337M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FLB_651133500EDR_F0822176FHAZ00337M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FLB_651133431EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FRB_651133431EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FLB_651133163EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FRB_651133163EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FLB_651132981EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FRB_651132981EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FRB_651132804EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/fcam/FLB_651132804EDR_F0822176FHAZ00206M_.JPG',
+        camera: 'Front Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/rcam/RLB_651133547EDR_F0822176RHAZ00337M_.JPG',
+        camera: 'Rear Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02857/opgs/edr/rcam/RRB_651133547EDR_F0822176RHAZ00337M_.JPG',
+        camera: 'Rear Hazard Avoidance Camera'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003315C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003314C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003313C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003312C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003311C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003310C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140021003309C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140011003308C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140001003307C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060021003306C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060011003305C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060001003304C00_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003315I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003314I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003313I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003312I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530011003311I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0005530001003310I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140021003309I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140011003308I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0008140001003307I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060021003306I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060011003305I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      },
+      {
+        src: 'https://mars.nasa.gov/msl-raw-images/msss/02857/mhli/2857MH0007060001003304I01_DXXX.jpg',
+        camera: 'Mars Hand Lens Imager'
+      }
+    ],
+    manifest: {
+      name: 'Curiosity',
+      landing_date: '2012-08-06',
+      launch_date: '2011-11-26',
+      status: 'active',
+      max_sol: 2857,
+      max_date: '2020-08-19'
+    }
+  }
